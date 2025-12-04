ca6dec90034873fa174739ae8a161582f5e50e27
Tyler Wu <tyler_wu@berkeley.edu>
2025-12-04 00:55:08 -0800

    fixed deprecated package issue

--- backend/vectordb.py
+++ backend/vectordb.py
@@ -1,8 +1,7 @@
-from langchain_community.vectorstores import Chroma
+from langchain_chroma import Chroma
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
-from langchain.schema import Document
 
 CHROMA_PATH = "chroma_db"
 
